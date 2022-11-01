d91c52a2313c3ea133ed87b974b5ece7fe56cf76
Laurent Trillaud <laurent.trillaud@everial.com>
2022-11-01 09:18:44 +0100

    feat(heater): add heater cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import {MatButtonModule} from '@angular/material/button'
-import {MatIconModule} from '@angular/material/icon'
-import {MatToolbarModule} from '@angular/material/toolbar'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -20,7 +22,9 @@ import { HeaterComponent } from './heater/heater.component'
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
+    FlexLayoutModule,
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatToolbarModule,
   ],
--- src/app/heater/heater.component.ts
+++ src/app/heater/heater.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+type HeaterType = 'S' | 'R' | 'A' | 'M' | 'P'
+interface Heater {
+  key: HeaterType
+  label: {
+    line1: string
+    line2: string
+  }
+}
+
 @Component({
   selector: 'app-heater',
   templateUrl: './heater.component.html',
   styleUrls: ['./heater.component.scss']
 })
 export class HeaterComponent implements OnInit {
-
+  heaters: Heater[] = [
+    { key: 'S', label: { line1: 'Salon et', line2: 'Séjour' } },
+    { key: 'P', label: { line1: 'Chambre', line2: 'Parentale' } },
+    { key: 'M', label: { line1: 'Chambre', line2: 'Maureen' } },
+    { key: 'A', label: { line1: 'Chambre', line2: 'Amandine' } },
+    { key: 'R', label: { line1: 'Salle de', line2: 'bain' } },
+  ]
   constructor() { }
 
   ngOnInit(): void {
